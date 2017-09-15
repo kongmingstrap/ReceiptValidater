f57875c6eb09e2517181e1941c17c4f695a5fa64
cm-tanaka.takaaki <tanaka.takaaki@classmethod.jp>
2017-09-15 11:39:53 +0900

    Initial Commit

---
+++ ReceiptValidater/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReceiptValidater
+//
+//  Created by tanaka.takaaki on 2017/09/15.
+//  Copyright © 2017年 tanaka.takaaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
